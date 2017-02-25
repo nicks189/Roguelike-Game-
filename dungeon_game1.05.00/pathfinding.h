b9a81797e1dfc57da5bd9c09555b455093590bd4
Nicholas J Steger <nsteger189@gmail.com>
2017-02-24 18:47:11 -0600

    Added files
    Game runs on its own - no user input yet.

---
+++ dungeon_game1.05.00/pathfinding.h
@@ -0,0 +1,16 @@
+#ifndef PATHFINDING_H
+#define PATHFINDING_H
+
+#include "dungeon.h"
+
+#define TUNNEL_MODE 0
+#define NON_TUNNEL_MODE 1
+
+int32_t weigh_hardness(uint8_t h);
+void pathfinding(_dungeon *d, uint8_t x_to, uint8_t y_to, corridor_path_t temp_path[DUNGEON_Y][DUNGEON_X], uint8_t mode);
+void print_maps(_dungeon *d, uint8_t l);
+void dijkstra_corridor(_dungeon *d, pair_t from, pair_t to);
+void dijkstra_corridor_inv(_dungeon *d, pair_t from, pair_t to);
+int32_t corridor_path_cmp(const void *key, const void *with);
+
+#endif
