ca3adb883c316f678f67cd507b085ef53ea54524
Nicholas J Steger <nsteger189@gmail.com>
2017-03-24 16:33:37 -0500

    Delete character_util.c

--- rlg/character_util.c
+++
@@ -1,8 +0,0 @@
-#include "character_util.h"
-#include "pathfinding.h"
-#include "event.h"
-
-int32_t event_cmp(const void *key, const void *with) {
-  return ((event_t *) key)->time - ((event_t *) with)->time;
-}
-
